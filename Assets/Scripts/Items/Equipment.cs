207a31af4031b60a176aa089ae35e219645e16ce
Christopher Miller <admin@mctherealm.net>
2018-11-01 14:37:39 -0400

    Began Item Classes

---
+++ Assets/Scripts/Items/Equipment.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equipment : Item {
+
+	public enum Slot { Hat, Face, Shirt, Arm, ArmNoWeapon, Module, Leg, Pants, Boots };
+
+    public Slot equipmentSlot;
+    public float armorValue;
+}
---
+++ Assets/Scripts/Items/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Item
+{
+    public enum Handed { One, Two };
+    public enum ShotType { Continuous, Bullet };
+    public enum ShotLength {  Semi, Burst, Auto };
+
+    public int fireRate;
+
+    public Handed handedness;
+    public ShotType shotType;
+
+    public int burstAmount;
+    public ShotLength shotLength;
+
+    public float damagePerSecond;
+    
+}
