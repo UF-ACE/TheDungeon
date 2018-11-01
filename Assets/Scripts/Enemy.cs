c9f0e634b55a108accbf71e3c3078b2aae02dfa2
Andrew Sanchez <andrewsanchez@ufl.edu>
2018-11-01 14:25:53 -0400

    Added base enemy and Enviro-Objects classes

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : Entity
+{
+
+
+    public Enemy(string name, float health, float maxHealth) : base(name, health, maxHealth)
+    { }
+
+    public abstract void AI();
+
+
+
+
+}
---
+++ Assets/Scripts/EnvironmentObjects.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnvironmentObjects : Entity {
+
+    public EnvironmentObjects(string name, float health, float maxHealth) : base(name, health, maxHealth)
+    { }
+
+}
